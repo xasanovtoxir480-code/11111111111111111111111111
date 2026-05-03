7ad568050f9f9178337fd34bb70879bd3fde71df
Z User <z@container>
2026-05-03 08:23:21 +0000

    446c26a4-0b52-4ab5-8b71-50acd3c189c0

--- start.sh
+++ start.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 cd /home/z/my-project
+# Ensure static files are linked in standalone
+cp -r .next/static .next/standalone/.next/static 2>/dev/null
 while true; do
   DATABASE_URL="file:/home/z/my-project/db/custom.db" NODE_ENV=production node .next/standalone/server.js
   sleep 2
